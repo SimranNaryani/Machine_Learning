fab67a8a7e7735792bc19084f5df6c0e5755f2ca
Simran Naryani <79084332+SimranNaryani@users.noreply.github.com>
2022-12-01 19:22:40 +0530

    Create Prediction_model.py

---
+++ House_price_prediction/Prediction_model.py
@@ -0,0 +1,78 @@
+# Dataset used: Boston Housing Dataset
+
+import numpy as np 
+import pandas as pd 
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+Columns:
+CRIM - per capita crime rate by town
+ZN - proportion of residential land zoned for lots over 25,000 sq.ft.
+INDUS - proportion of non-retail business acres per town.
+CHAS - Charles River dummy variable (1 if tract bounds river; 0 otherwise)
+NOX - nitric oxides concentration (parts per 10 million)
+RM - average number of rooms per dwelling
+AGE - proportion of owner-occupied units built prior to 1940
+DIS - weighted distances to five Boston employment centres
+RAD - index of accessibility to radial highways
+TAX - full-value property-tax rate per $10,000
+PTRATIO - pupil-teacher ratio by town
+B - 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+LSTAT - % lower status of the population
+MEDV - Median value of owner-occupied homes in $1000's
+"""
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+data = pd.read_csv('housing.csv', delimiter = r"\s+", names = column_names) # \s+ means any one or more white space characters
+
+data.head(5) # Getting first 5 rows
+
+print(np.shape(data)) # Dimensions of data
+
+data.describe() # Summarize the data
+
+# Plotting to see trends in data
+fig, axs = plt.subplots(ncols=7, nrows=2, figsize=(20, 10))
+index = 0
+axs = axs.flatten()
+
+# Plotting to check outliers
+for k,v in data.items():
+    sns.boxplot(y=k, data=data, ax=axs[index])
+    index += 1
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=5.0) 
+
+# Values above 50.00 may not help to predict MEDV hence, remove them
+data = data[~(data['MEDV'] >= 50.0)] 
+print(np.shape(data))
+
+# Linear regression
+from sklearn import datasets, linear_model
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn import preprocessing
+
+l_regression = linear_model.LinearRegression()
+kf = KFold(n_splits=10)
+min_max_scaler = preprocessing.MinMaxScaler()
+
+# Trying to predict MEDV with 'LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE' features
+column_sels = ['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+x = data.loc[:,column_sels]
+y = data['MEDV']
+x = pd.DataFrame(data=min_max_scaler.fit_transform(x), columns=column_sels)
+
+x_scaled = min_max_scaler.fit_transform(x)
+scores = cross_val_score(l_regression, x_scaled, y, cv=kf, scoring='neg_mean_squared_error')
+print("MSE: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+
+# Linear ridge regression
+scores_map = {}
+scores_map['LinearRegression'] = scores
+l_ridge = linear_model.Ridge()
+scores = cross_val_score(l_ridge, x_scaled, y, cv=kf, scoring='neg_mean_squared_error')
+scores_map['Ridge'] = scores
+print("MSE: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
